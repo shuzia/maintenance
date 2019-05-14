7ee2fe2663b9dd413581a47f9efd1809d3378266
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2019-05-14 21:27:51 +0100

    maintenance web for shuzia

---
+++ shuzia.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<head>
+<!-- Website Template designed by www.downloadwebsitetemplates.co.uk -->
+<meta charset="UTF-8">
+<title>Shuzia</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<link rel="shortcut icon" href="favicon.ico">
+<!--[if IE]><![endif]-->
+<link rel="stylesheet" href="scss/style.css">
+<script src="sjs/jquery.js"></script>
+<script src="sjs/countdown.js"></script>
+<script src="sjs/owlcarousel.js"></script>
+<script src="sjs/uikit.scrollspy.js"></script>
+<script src="sjs/scripts.js"></script>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body id="backtotop">
+
+<div class="fullwidth clearfix">
+	<div id="topcontainer" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
+		
+		<p><span class="fa fa-cogs fa-spin"></span></p>
+		<h1><span>shuzia.com</span><br />is undergoing maintenance</h1>
+		<p>It's almost ready ... honest</p>
+		<p>You can still visit us on <a href="https://beta.shuzia.net" rel="external"><strong>https://beta.shuzia.com</strong></a></p>
+		
+	</div>
+</div>
+
+<div class="arrow-separator arrow-white"></div>
+
+<div class="fullwidth colour1 clearfix">
+	<div id="countdown" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
+
+		<div id="countdowncont" class="clearfix">
+			<ul id="countscript">
+				<li>
+					<span class="days">00</span>
+					<p>Days</p>
+				</li>
+				<li>
+					<span class="hours">00</span>
+					<p>Hours</p>
+				</li>
+				<li class="clearbox">
+					<span class="minutes">00</span>
+					<p>Minutes</p>
+				</li>
+				<li>
+					<span class="seconds">00</span>
+					<p>Seconds</p>
+				</li>
+			</ul>
+		</div>
+	
+	</div>
+</div>
+   
+</body>
+</html>
